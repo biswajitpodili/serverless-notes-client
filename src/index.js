76f45476b6f25109c5275e4497d5b82b9e41b2e9
Biswajit <biswajitofficial2002@gmail.com>
2024-05-05 17:23:26 +0530

    Complete Project

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { Amplify } from "@aws-amplify/core";
+import { Amplify } from 'aws-amplify';
 import { BrowserRouter as Router } from "react-router-dom";
 import config from "./config";
 
